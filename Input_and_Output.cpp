e9bdd10461ae545a0754bf7b590b479fd05d2512
Ahamed91 <ahammedrimshan7469@gmail.com>
2023-12-27 21:56:18 +0530

    Created Input_and_Output file

--- Input_and_Output.cpp
+++ Input_and_Output.cpp
@@ -1,1 +1,15 @@
+/**********************Author-Ahamed*****************************/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c; //This line will read inputs from stdin
+    cout<<a+b+c; 
+    return 0;
+}
